60a12c454e5339e62cd09a4e874e356c3f2f9f6b
Sweetsan <coderintheworldhack100@gmail.com>
2026-06-09 22:45:41 +0530

    inside of  models folder init .py is updated

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from app.models.student import Student
 from app.models.lecture import Lecture
 from app.models.course import Course
+from app.models.enrollment import Enrollment
 
-__all__ = ["Student", "Lecture", "Course"]
+__all__ = ["Student", "Lecture", "Course", "Enrollment"]
