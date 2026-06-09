7a496bda75c61a8bafdfe0cfcbbca878f136259e
Sweetsan <coderintheworldhack100@gmail.com>
2026-06-09 23:18:30 +0530

    inside of  routes folder init.py file is updated

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,9 +1,11 @@
 from app.routes.student_routes import student_bp
 from app.routes.lecture_routes import lecture_bp
 from app.routes.course_routes import course_bp
+from app.routes.enrollment_routes import enrollment_bp
 
 
 def register_blueprints(app):
     app.register_blueprint(student_bp)
     app.register_blueprint(lecture_bp)
     app.register_blueprint(course_bp)
+    app.register_blueprint(enrollment_bp)
