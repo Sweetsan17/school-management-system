bd835137f953c722acdf261ab7e37721529fc77d
Sweetsan <coderintheworldhack100@gmail.com>
2026-06-09 20:23:14 +0530

    inside of routes init.py file code is  write

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,7 @@
 from app.routes.student_routes import student_bp
+from app.routes.lecture_routes import lecture_bp
 
 
 def register_blueprints(app):
     app.register_blueprint(student_bp)
+    app.register_blueprint(lecture_bp)
