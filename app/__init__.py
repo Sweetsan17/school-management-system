f3c89ffa3c66515fbbd84a443770a7b451a4e179
Sweetsan <coderintheworldhack100@gmail.com>
2026-06-09 06:53:06 +0530

    init.py changed

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ def create_app():
     app.config.from_object(Config)
     db.init_app(app)
 
-    # from app.models import Course, Student  # noqa: F401
+    from app.models import student  # noqa: F401
 
     register_blueprints(app)
 
